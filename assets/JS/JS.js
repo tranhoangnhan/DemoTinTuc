36a7b069046d8f1a9e02d8b3d45f1c6767497f27
Trần Hoàng Nhân <58924621+tranhoangnhan@users.noreply.github.com>
2022-06-07 21:33:30 +0700

    Boss
    Updare Rì Pon Siu

---
+++ assets/JS/JS.js
@@ -0,0 +1,71 @@
+//  Slider TEXT
+$(document).ready(function () {
+
+    $('.autoplay').slick({
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        Accessibility: true,
+        appendArrows: '.contron-pre-next',
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
+});
+// end Slider TEXT
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+
+$(document).ready(function() {
+    // Show or hide the sticky footer button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.go-top').fadeIn(200);
+        } else {
+            $('.go-top').fadeOut(200);
+        }
+    });
+    
+    // Animate the scroll to top
+    $('.go-top').click(function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({scrollTop: 0}, 300);
+    })
+});
